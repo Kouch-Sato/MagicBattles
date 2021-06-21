9c79b0932f7878e6ba8b94e375e634e33017e87a
Kouch-Sato <redkouchred@gmail.com>
2021-06-21 17:59:11 +0900

    ops

--- Assets/Script/EnemyManager.cs
+++ Assets/Script/EnemyManager.cs
@@ -1,27 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyManager : MonoBehaviour
 {
     public int maxHP;
     int hp;
 
+    Transform target;
+    NavMeshAgent agent;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         hp = maxHP;
+        target = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        agent.destination = target.position;
+        animator.SetFloat("Distance", agent.remainingDistance);
     }
 
     public void GetDamage(int damage)
     {
-        Debug.Log("aaaaaa");
         hp -= damage;
         Debug.Log("HP:" + hp);
 
