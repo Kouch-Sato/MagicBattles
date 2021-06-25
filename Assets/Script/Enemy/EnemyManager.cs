9bda0b3c6d09aa5c579d4aaa9a63718bb2ba024a
Kouch-Sato <redkouchred@gmail.com>
2021-06-25 18:16:51 +0900

    Battle02

--- Assets/Script/Enemy/EnemyManager.cs
+++ Assets/Script/Enemy/EnemyManager.cs
@@ -1,36 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyManager : MonoBehaviour
 {
     public int maxHP;
     int HP;
 
-    public Collider weaponCollider;
-
-    Transform target;
-    NavMeshAgent agent;
     Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         HP = maxHP;
-        target = GameObject.FindWithTag("Player").transform;
-        agent = GetComponent<NavMeshAgent>();
-        agent.destination = target.position;
         animator = GetComponent<Animator>();
-
-        HideColliderWeapon();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = target.position;
-        animator.SetFloat("Distance", agent.remainingDistance);
+
     }
 
     public void GetDamage(int damage)
@@ -49,15 +38,5 @@ public class EnemyManager : MonoBehaviour
     {
         animator.SetTrigger("Die");
         Destroy(gameObject, 2f);
-    }
-
-    public void ShowColliderWeapon()
-    {
-        weaponCollider.enabled = true;
-    }
-
-    public void HideColliderWeapon()
-    {
-        weaponCollider.enabled = false;
     }
 }
