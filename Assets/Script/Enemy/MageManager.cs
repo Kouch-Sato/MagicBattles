d0027f9ac70a9961dccda1b7ec518481cdccaaea
Kouch-Sato <redkouchred@gmail.com>
2021-06-25 19:22:38 +0900

    behavior

--- Assets/Script/Enemy/MageManager.cs
+++ Assets/Script/Enemy/MageManager.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerGetDamage : MonoBehaviour
+public class MageManager : MonoBehaviour
 {
+    Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
