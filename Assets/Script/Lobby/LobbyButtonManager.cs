df9fc43f996bab488adad113231c39b950557347
kota-shidara <3t.u1.kota@gmail.com>
2021-06-29 13:35:24 +0900

    lobby script

---
+++ Assets/Script/Lobby/LobbyButtonManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyButtonManager : MonoBehaviour
+{
+    public int targetStageLevel;
+    public string targetStageName;
+    
+}
