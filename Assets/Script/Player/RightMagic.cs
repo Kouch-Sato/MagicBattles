6518f5a1ef75332f9235161624f13acaf117b652
Kouch-Sato <redkouchred@gmail.com>
2021-07-01 12:23:29 +0900

    ops

--- Assets/Script/Player/RightMagic.cs
+++ Assets/Script/Player/RightMagic.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RightMagic : MonoBehaviour
 {
     public GameObject magicPrefab;
+    public GameObject rainMagicPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,12 @@ public class RightMagic : MonoBehaviour
         if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
         {
             Attack();
+            RainAttack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            RainAttack();
         }
     }
 
@@ -30,5 +37,11 @@ public class RightMagic : MonoBehaviour
     {
         GameObject magicGameObject = Instantiate(magicPrefab, transform.position, transform.rotation) as GameObject;
         magicGameObject.GetComponent<Rigidbody>().AddForce(magicGameObject.transform.forward * 1000);
+    }
+
+    private void RainAttack()
+    {
+        Instantiate(rainMagicPrefab, transform.position, transform.rotation);
+        Debug.Log(11111);
     }
 }
