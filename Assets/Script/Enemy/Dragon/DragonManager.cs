dad9d17e4cb8dd734634bc02a13d70abd52474d9
Kouch-Sato <redkouchred@gmail.com>
2021-07-03 14:14:58 +0900

    enemy

---
+++ Assets/Script/Enemy/Dragon/DragonManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DragonManager : MonoBehaviour
+{
+    public GameObject magicPrefab;
+    public GameObject bodyPrefab;
+    Transform target;
+    NavMeshAgent agent;
+    Animator animator;
+    GameObject magicGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+        target = GameObject.FindWithTag("Player").transform;
+        this.transform.LookAt(target.position);
+        animator.SetFloat("Distance", agent.remainingDistance);
+    }
+
+    public void StartAttack()
+    {
+        magicGameObject = Instantiate(magicPrefab, bodyPrefab.transform.position, transform.rotation) as GameObject;
+        magicGameObject.transform.LookAt(target.position + new Vector3(0, -1.0f, 0));
+    }
+
+    public void StopAttack()
+    {
+        Destroy(magicGameObject);
+    }
+}
