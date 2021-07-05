2f106fab678a5bb5cadf50bda3506712e775f8b2
kota-shidara <3t.u1.kota@gmail.com>
2021-07-05 15:52:18 +0900

    change

--- Assets/Script/Enemy/Skeleton/SkeletonGetHitBehaviour.cs
+++ Assets/Script/Enemy/Skeleton/SkeletonGetHitBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyGetHitBehaviour : StateMachineBehaviour
+public class SkeletonGetHitBehaviour : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Assets/Script/Enemy/Skeleton/SkeletonIdleBehaviour.cs
+++ Assets/Script/Enemy/Skeleton/SkeletonIdleBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyIdleBehaviour : StateMachineBehaviour
+public class SkeletonIdleBehaviour : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
