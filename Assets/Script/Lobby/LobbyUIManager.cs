b3f3b62f1b9e74d7656f3ae36cd000d4f7454fde
kota-shidara <3t.u1.kota@gmail.com>
2021-07-10 17:38:16 +0900

    jyannzeri

--- Assets/Script/Lobby/LobbyUIManager.cs
+++ Assets/Script/Lobby/LobbyUIManager.cs
@@ -5,38 +5,45 @@ using UnityEngine.UI;
 
 public class LobbyUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Color unlockedStageColor = new Color(224f / 255f, 255f / 255f, 255f / 255f, 255f/255f);
-    Color lockedStageColor = new Color(169f / 255f, 169f / 255f, 169f / 255f, 255f/255f);
+    Color lockedButtonColor = new Color(147f / 255f, 147f / 255f, 147f / 255f, 255f/255f);
+    Color lockedButtonTextColor = new Color(144f / 255f, 107f / 255f, 40f / 255f, 255f/255f);
     void Start()
     {
-        // PlayerPrefs.SetInt("PLAYER_LEVEL", 1);
+        // PlayerPrefs.SetInt("PLAYER_LEVEL", 3);
         // PlayerPrefs.Save();
-        // Debug.Log(PlayerPrefs.GetInt("PLAYER_LEVEL", 0));
 
         int playerLevel = PlayerPrefs.GetInt("PLAYER_LEVEL", 0);
 
+        SetLobbyStageButton(playerLevel);
+
+        GameObject playerLevelText = GameObject.Find("PlayerLevelText");
+        playerLevelText.GetComponent<Text>().text = "レベル: " + playerLevel.ToString();
+    }
+
+    void SetLobbyStageButton(int playerLevel)
+    {
         GameObject[] lobbyStageButtons = GameObject.FindGameObjectsWithTag("LobbyStageButton");
         foreach(GameObject button in lobbyStageButtons)
         {
             int targetStageLevel = button.GetComponent<LobbyButtonManager>().targetStageLevel;
+            int stageScore = PlayerPrefs.GetInt($"SCORE_{targetStageLevel}", 0);
+
             if (targetStageLevel <= playerLevel)
             {
-                button.GetComponent<Image>().color = unlockedStageColor;
+                button.transform.Find($"Star_{stageScore}").gameObject.SetActive(true);
             }
             else if (targetStageLevel == playerLevel + 1)
             {
-                button.GetComponent<Image>().color = unlockedStageColor;
-                button.GetComponentInChildren<Text>().fontStyle = FontStyle.Bold;
+                button.transform.Find("Unlock").gameObject.SetActive(true);
             }
             else
             {
-                button.GetComponent<Image>().color = lockedStageColor;
+                button.GetComponent<Image>().color = lockedButtonColor;
+                button.transform.Find("Text_Normal").GetComponent<Text>().color = lockedButtonTextColor;
+                button.transform.Find("Text_Pressed").GetComponent<Text>().color = lockedButtonTextColor;
                 button.GetComponent<Button>().interactable = false;
+                button.transform.Find("Lock").gameObject.SetActive(true);
             }
         }
-
-        GameObject playerLevelText = GameObject.Find("PlayerLevelText");
-        playerLevelText.GetComponent<Text>().text = "レベル: " + playerLevel.ToString();
     }
 }
