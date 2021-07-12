ceb359ebf041da227f743994ff12aa6466b85a72
Kouch-Sato <redkouchred@gmail.com>
2021-07-12 11:31:48 +0900

    script

--- Assets/Script/Enemy/BossDragon/BossDragonManager.cs
+++ Assets/Script/Enemy/BossDragon/BossDragonManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossDragonManager : MonoBehaviour
 {
@@ -9,12 +10,18 @@ public class BossDragonManager : MonoBehaviour
     Transform target;
     Animator animator;
     GameObject magicGameObject;
+    float distance;
+    bool isFlying;
+    NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindWithTag("Player").transform;
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+        agent.speed = 0;
     }
 
     // Update is called once per frame
@@ -22,13 +29,32 @@ public class BossDragonManager : MonoBehaviour
     {
         target = GameObject.FindWithTag("Player").transform;
         this.transform.LookAt(target.position);
+        distance = Vector3.Distance(target.position, transform.position);
 
         if (magicGameObject)
         {
             magicGameObject.transform.position = head.transform.position;
-            // magicGameObject.transform.LookAt(target.position);
             magicGameObject.transform.rotation = head.transform.rotation;
             magicGameObject.transform.Rotate(10, 0, 0);
+        }
+
+        if (distance > 20)
+        {
+            isFlying = true;
+            animator.SetTrigger("TakeOff");
+        }
+
+        if (isFlying)
+        {
+            if (distance < 10)
+            {
+                agent.speed = 0;
+                animator.ResetTrigger("TakeOff");
+                animator.SetTrigger("FlyFlame");
+                isFlying = false;
+            }
+
+            agent.speed = 5;
         }
     }
 
